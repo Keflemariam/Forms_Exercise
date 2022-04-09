f5d10be96c5f9111aa74f51a899e6cca836f3402
Keflemariam <keflemariam@outlook.com>
2022-04-09 11:24:02 +0300

    add user interface and service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,22 @@
+import { User } from '../Interface/user';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  apiURL="https://jsonplaceholder.typicode.com/users";
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
+  getUsers():Observable<User[]>{
+    return this.http.get<User[]>(this.apiURL);
+  }
+
+  getUser():Observable<User>{
+    return this.http.get<User>(this.apiURL+'/1')
+  }
 }
